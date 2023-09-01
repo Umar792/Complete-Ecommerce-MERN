c136655190f502763e71ba8c7585ac17a4c54c49
Muhammad Umar Yousaf <106830172+Umar792@users.noreply.github.com>
2023-09-01 23:36:52 +0500

    Small_changes_about_product_card

--- cliten/src/Component/Home/ProductCrad.jsx
+++ cliten/src/Component/Home/ProductCrad.jsx
@@ -3,29 +3,68 @@ import { Link, NavLink } from "react-router-dom";
 
 const ProductCrad = ({ product }) => {
   return (
-    <Link className="productCard" to={`/singleProduct/${product._id}`}>
-      {/* <NavLink to={`/singleProduct/${product._id}`}> */}
-      {product && product.percentageDiscount && (
-        <span className="off">{`-${
-          product && product.percentageDiscount
-        }%`}</span>
-      )}
+    // <Link className="productCard" to={`/singleProduct/${product._id}`}>
+    //   {product && product.percentageDiscount && (
+    //     <span className="off">{`-${
+    //       product && product.percentageDiscount
+    //     }%`}</span>
+    //   )}
 
-      <img
-        src={product && product.images && product.images[0].url}
-        alt={product && product.name}
-      />
-      <div className="pricing flex justify-between place-items-end">
-        <span>{`$${product.price}`}</span>
-        <p className="disount">${product && product.Discountprice}</p>
+    //   <img
+    //     src={product && product.images && product.images[0].url}
+    //     alt={product && product.name}
+    //   />
+    //   <div className="pricing flex justify-between place-items-end">
+    //     <span>{`$${product.price}`}</span>
+    //     <p className="disount">${product && product.Discountprice}</p>
+    //   </div>
+    //   <p>{product && product.name && product.name.slice(0, 30)}...</p>
+    //   <font className="text-[green]">100% Free Sgipping</font>
+    //   <a href={product && product.link && product.link} target="_blank">
+    //     <button>Shop Now</button>
+    //   </a>
+    // </Link>
+    <>
+      <div className="new_product_card m-[20px] card w-64 bg-base-100 shadow-xl">
+        <Link className="" to={`/singleProduct/${product._id}`}>
+          <figure>
+            <img
+              className="w-[100%] h-[250px] object-fill"
+              src={product && product.images && product.images[0].url}
+              alt={product && product.name}
+            />
+          </figure>
+        </Link>
+
+        <div className="card-body">
+          {/* <h2 className="card-title">{}</h2> */}
+          <div className="pricing flex justify-start mb-1 place-items-end">
+            <Link
+              className="flex place-items-center"
+              to={`/singleProduct/${product._id}`}
+            >
+              <span>{`$${product.price}`}</span>
+              <p className="disount">${product && product.Discountprice}</p>
+            </Link>
+          </div>
+          <Link
+            className="flex place-items-center"
+            to={`/singleProduct/${product._id}`}
+          >
+            <p className="mb-1">
+              {product && product.name && product.name.slice(0, 25)}...
+            </p>
+          </Link>
+          <div className="card-actions justify-end">
+            <a href={product && product.link} rel="noreferrer" target="_blank">
+              <button className="btn mt-2 bg-[#FF4747] text-white hover:bg-[#FF4747]">
+                Buy Now
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
-      <p>{product && product.name && product.name.slice(0, 30)}...</p>
-      {/* </NavLink> */}
-      <font className="text-[green]">100% Free Sgipping</font>
-      <a href={product && product.link && product.link} target="_blank">
-        <button>Shop Now</button>
-      </a>
-    </Link>
+    </>
   );
 };
 
